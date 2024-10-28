bc2187c822b78964d49188a4b91cb55df6acfedc
aochoa21 <aochoa21@students.avc.edu>
2024-10-27 21:33:43 -0700

    Done quiz question 1

--- main.hpp
+++ main.hpp
@@ -1,8 +1,46 @@
+#include <iostream>
+#include <limits>
+using namespace std;
 
-//********************
-// Declare Function Prototypes
-//********************
+void getTwoValues(int &, int &);
+bool isPrime(int);
+int getNextPrime(int);
+int getPrevPrime(int);
 
-// ******************************
-// Implement all your functions here
-// ******************************
+void getTwoValues(int &begin, int &end){
+    while(true){
+        cout << "Enter two integers (begin < end): ";
+        cin >> begin >> end;
+        if (begin < end){
+            break;
+        }
+        else {
+            cout << "Invalid input. Please make sure first input is less than the second input." << endl;
+        }
+    }
+}
+bool isPrime(int num){
+    if(num <= 1){
+        return false;
+    }
+    for (int i = 2; i * i <+ num; i++){
+        if(num % i == 0){
+            return false;
+        }
+    }
+    return true;
+}
+int getNextPrime(int begin){
+    int next = begin + 1;
+    while (!isPrime(next)){
+        next++;
+    }
+    return next;
+}
+int getPrevPrime(int end){
+    int prev = end - 1;
+    while (prev > 1 && !isPrime(prev)){
+        prev--;
+    }
+    return prev;
+}
